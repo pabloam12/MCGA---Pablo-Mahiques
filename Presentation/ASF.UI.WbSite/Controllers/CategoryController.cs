2301f0502c22a903bc623890e8ab4fec8d2775ab
pabloam12 <pablo.a.mahiques@gmail.com>
2017-09-01 21:13:06 -0300

    Carga Inicial

---
+++ Presentation/ASF.UI.WbSite/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using ASF.Entities;
+using ASF.UI.Process;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASF.UI.WbSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            //TEST
+            var cp = new CategoryProcess();
+            return View(cp.SelectList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            var cp = new CategoryProcess();
+            cp.insertCategory(category);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
